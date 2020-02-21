7d38380f1e8ff8fee6f72f33d527a2d39bb2d01d
Maxime Salomon <maxime@croissant.io>
2020-02-22 00:42:10 +0100

    add schema

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,8 @@
-// TO DO
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
