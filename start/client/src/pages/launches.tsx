5cadc6b83b907333a24c32556894abaed35389b9
Maxime Salomon <maxime@croissant.io>
2020-02-22 23:30:00 +0100

    connect to API w/ Apollo Client

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,11 +1,28 @@
 import React, { Fragment }  from 'react';
 import { RouteComponentProps } from '@reach/router';
+import gql from "graphql-tag";
 
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
   return <div />;
 }
+
+export const LAUNCH_TILE_DATA = gql`
+  fragment LaunchTile on Launch {
+    __typename
+    id
+    isBooked
+    rocket {
+      id
+      name
+    }
+    mission {
+      name
+      missionPatch
+    }
+  }
+`;
 
 export default Launches;
 
